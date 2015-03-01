bba16a49150ab2f8fb0871ecfaaa42f99af94e5b
Gregor Richards <hg-yff@gregor.im>
2015-02-28 23:15:54 -0500

    Removing deggggc and noggggc, which never worked WELL anyway.

--- deggggc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-BASE=`dirname "$0"`
-while [ "$1" ]
-do
-    ( printf '%s\n' '@BEGIN' ; sed 's/^#/@/' "$1" ) |
-        gcc -E -C -imacros "$BASE/ggggc/noggggc.h" - |
-        sed -n '/^@BEGIN/ { s///; :l; n; /^#/ {d; bl}; s/^@/#/; s/ggggc\/gc.h/ggggc\/noggggc.h/; p; bl }'
-    shift
-done
