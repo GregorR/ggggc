a0e63a445506bbcac90341c019bb4b5080a0da9d
Gregor Richards <hg-yff@gregor.im>
2015-09-20 12:35:30 -0400

    Initialize our mutexes correctly on OS X.

--- globals.c
+++ globals.c
@@ -7,8 +7,8 @@ ggc_thread_local struct GGGGC_PointerStack *ggggc_pointerStack, *ggggc_pointerSt
 volatile int ggggc_stopTheWorld;
 ggc_barrier_t ggggc_worldBarrier;
 ggc_size_t ggggc_threadCount;
-ggc_mutex_t ggggc_worldBarrierLock;
-ggc_mutex_t ggggc_rootsLock;
+ggc_mutex_t ggggc_worldBarrierLock = GGC_MUTEX_INITIALIZER;
+ggc_mutex_t ggggc_rootsLock = GGC_MUTEX_INITIALIZER;
 struct GGGGC_PoolList *ggggc_rootPool0List;
 struct GGGGC_PointerStackList *ggggc_rootPointerStackList;
 struct GGGGC_PoolList *ggggc_blockedThreadPool0s;
