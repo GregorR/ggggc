a1be17c86ece5722fb0ff526fd1ff2bf7a083b4d
Gregor Richards <hg-yff@gregor.im>
2015-09-22 22:26:31 -0400

    Updating copyright year in headers.

--- ggggc/threads-macosx.h
+++ ggggc/threads-macosx.h
@@ -1,7 +1,7 @@
 /*
  * Thread support for Mac OS X's awful almost-pthreads
  *
- * Copyright (c) 2014 Gregor Richards
+ * Copyright (c) 2014, 2015 Gregor Richards
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
