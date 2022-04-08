bad2978a10a53e09fba18062d2340a2eadb7f7a7
Gregor Richards <hg-yff@gregor.im>
2022-04-07 23:30:09 -0400

    With portablems, we need much less in dos16.h

--- ggggc/dos16.h
+++ ggggc/dos16.h
@@ -1,11 +1,9 @@
 #ifndef GGGGC_DOS16_H
 #define GGGGC_DOS16_H 1
 
+#ifndef GGGGC_NO_THREADS
 #define GGGGC_NO_THREADS 1
-
-#define GGGGC_GENERATIONS 1
-#define GGGGC_POOL_SIZE 16
-#define GGGGC_CARD_SIZE 10
+#endif
 
 #define ggc_size_t unsigned long
 
