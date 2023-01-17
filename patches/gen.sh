41f49f5c0557e9dd0195c59b30bdbe1b38d33156
Gregor Richards <hg-yff@gregor.im>
2023-01-17 14:43:42 -0500

    New patch: jitpstack-exttag

--- patches/gen.sh
+++ patches/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for i in bytetags finalizers jitpstack tagging
+for i in bytetags finalizers jitpstack jitpstack-exttag tagging
 do
     git diff origin/master patch-$i -- > $i/$i.diff
 done
